40585ee776ce290ff2cf44adaf755bdbb60a8159
pzoladzi <pzoladzi@intercars.eu>
2024-08-06 18:15:36 +0200

    25-testy parametryzowane, plik csv

---
+++ src/main/java/pl/devfoundry/testing/OrderStatus.java
@@ -0,0 +1,6 @@
+package pl.devfoundry.testing;
+
+public enum OrderStatus {
+
+    ORDERED, READY, DELIVERED
+}
--- src/test/java/pl/devfoundry/testing/MealTest.java
+++ src/test/java/pl/devfoundry/testing/MealTest.java
@@ -2,8 +2,19 @@ package pl.devfoundry.testing;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("Test cases for Meals")
@@ -19,7 +30,7 @@ class MealTest {
         int discountedPrice = meal.getDiscountedPrice(7);
 
         //then
-        assertEquals(28,discountedPrice);
+        assertEquals(28, discountedPrice);
         assertThat(discountedPrice).isEqualTo(28);
     }
 
@@ -31,7 +42,7 @@ class MealTest {
         Meal meal2 = meal1;
 
         //then
-        assertSame(meal1,meal2);
+        assertSame(meal1, meal2);
         assertThat(meal1).isSameAs(meal2);
     }
 
@@ -43,7 +54,7 @@ class MealTest {
         Meal meal2 = new Meal(10);
 
         //then
-        assertNotSame(meal1,meal2);
+        assertNotSame(meal1, meal2);
         assertThat(meal1).isNotSameAs(meal2);
     }
 
@@ -51,11 +62,11 @@ class MealTest {
     void twoMealsShouldBeEqualWhenPriceAndNameAreTheSame() {
 
         //given
-        Meal meal1 = new Meal(10,"Pizza");
-        Meal meal2 = new Meal(10,"Pizza");
+        Meal meal1 = new Meal(10, "Pizza");
+        Meal meal2 = new Meal(10, "Pizza");
 
         //then
-        assertEquals(meal1,meal2,"Checking if two meals are equal");
+        assertEquals(meal1, meal2, "Checking if two meals are equal");
         assertThat(meal1).isEqualTo(meal2);
     }
 
@@ -64,12 +75,44 @@ class MealTest {
     void exceptionShouldBeThrownIfDiscountIsHigherThanThePrice() {
 
         //given
-        Meal meal = new Meal(4,"Soup");
+        Meal meal = new Meal(4, "Soup");
 
         //when
         //then
         assertThrows(IllegalArgumentException.class, () -> meal.getDiscountedPrice(5));
     }
 
+    @ParameterizedTest
+    @ValueSource(ints = {5, 10, 15, 18})
+    void mealPricesShouldBeLowerThan20(int price) {
+        assertThat(price, lessThan(20));
+    }
 
+
+    @ParameterizedTest
+    @MethodSource("createMealsWithNameAndPrice")
+    void burgerShouldHaveCorrectNameAndPrice(String name, int price) {
+        assertThat(name, containsString("burger"));
+        assertThat(price, greaterThanOrEqualTo(10));
+    }
+
+    //metoda pomocnicza statyczna, ta metoda bedzie zwracala strumien argumentow
+    private static Stream<Arguments> createMealsWithNameAndPrice() {
+        return Stream.of(
+                Arguments.of("Hamburger", 10),
+                Arguments.of("Cheeseburger", 12)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("createCakeNames")
+    void cakeNamesShouldEndWithCake(String name) {
+        assertThat(name, notNullValue());
+        assertThat(name, endsWith("cake"));
+    }
+
+    private static Stream<String> createCakeNames() {
+        List<String> cakeNames = Arrays.asList("Cheesecake","Fruitcake","Cupcake");
+        return cakeNames.stream();
+    }
 }
